438c02b4d46e498424a6b9d9c0143eab2d2bd586
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-12-17 00:26:30 -0500

    intermediate revision - removed the grammar regex generation code from the project

--- java/src/main/java/com/github/sergueik/selenium/CssValidator.java
+++ java/src/main/java/com/github/sergueik/selenium/CssValidator.java
@@ -28,57 +28,6 @@ public class CssValidator {
 		return instance;
 	}
 
-	// origin: CssSelectorStringSplitter.java
-	// The following appears, because of excessive grouping
-	// to neither work too well nor is really needed for task at hand
-
-	private static final String DOM_NAVS = " ~+>";
-	private static final String DOM_NAV_RE = "[" + DOM_NAVS + "]+"; // TODO: debug
-
-	// @formatter:off
-	private static final String ELEMENT_AND_CONDITION = "([^" + DOM_NAVS + "\\[]*(\\[[^\\]]+\\])*)";
-	private static final String ELEMENT_AND_CONDITION_FOLLOWED_BY_DOM_NAV_EXTRACTOR = "^" + ELEMENT_AND_CONDITION + "($|(\\s*(" + DOM_NAV_RE + ")\\s*" + "([^" + DOM_NAVS + "].*)$))";
-	// @formatter:on
-
-	private static final String ELEMENT_AND_CONDITION_FOLLOWED_BY_DOM_NAV_EXTRACTOR_FIXED = "^([^ ~+>\\[]*(?:\\[[^\\]]+\\])*)($|\\s*[ ~+>]\\s*[^ ~+>\\[].*$)";
-
-	// origin: CssElementAttributeParser.java
-	// The following appears, because of excessive grouping
-	// to neither work too well nor is really needed for task at hand
-	private static final String QUOTES_RE = "([\"\'])";
-	private static final String ATTRIBUTE_VALUE_RE = "([-_.#a-zA-Z0-9:\\/ ]+)";
-	private static final String ATTRIBUTE_VALUE_RE_NO_SPACES = "([-_.#a-zA-Z0-9:\\/]+)";
-	private static final String ATTRIBUTE_TYPE_RE = createElementAttributeNameRegularExpression();
-	private static final String ELEMENT_ATTRIBUTE_NAME_RE = "(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)";
-	// @formatter:off
-	private static final String STARTING_ELEMENT_RE = "^(" + ELEMENT_ATTRIBUTE_NAME_RE + "|([*]))?";
-	// @formatter:on
-	private static final String PSUEDO_RE = "(:[a-z][a-z\\-]*([(][^)]+[)])?)";
-	// @formatter:off
-	private static final String ATTRIBUTE_RE = "(" + PSUEDO_RE + "|(\\[" + "\\s*" + ELEMENT_ATTRIBUTE_NAME_RE + "\\s*" + ATTRIBUTE_TYPE_RE + "\\s*" + "((" + QUOTES_RE + ATTRIBUTE_VALUE_RE + QUOTES_RE + ")|(" + ATTRIBUTE_VALUE_RE_NO_SPACES + "))?" + "\\s*" + "\\]))";
-	// @formatter:on
-
-	// @formatter:off
-	private static final String CSS_CONDTION_EXTRACTOR = STARTING_ELEMENT_RE	+ ATTRIBUTE_RE + "*$";
-	// @formatter:on
-
-	private static String createElementAttributeNameRegularExpression() {
-		StringBuilder builder = new StringBuilder();
-		for (CssAttributeValueType type : CssAttributeValueType.values()) {
-			if (builder.length() == 0) {
-				builder.append("((");
-			} else {
-				builder.append(")|(");
-			}
-			builder.append("\\").append(type.getEqualStringName());
-		}
-		builder.append("))?");
-		// System.out.println("elementAttributeRE="+builder);
-		return builder.toString();
-	}
-
-	// Shortened grammar expression:
-
 //@formatter:off
 	private static final String CSS_CONDTION_EXTRACTOR_FIXED = "(?i)^(-?[_a-z]+[_a-z0-9-]*|\\*)?(#[_a-z0-9-]*)?(\\.[_a-z0-9-]*)?(:[a-z][a-z\\-]*\\([^)]+\\))?(\\[\\s*-?[_a-z]+[_a-z0-9-]*\\s*(\\=|\\~=|\\|=|\\^=|\\$=|\\*=)?\\s*([\"'][-_.#a-z0-9:\\/ ]+[\"']|[-_.#a-z0-9:\\/]+)?\\s*\\])*$";
 //@formatter:on
@@ -90,9 +39,12 @@ public class CssValidator {
 		this.debug = value;
 	}
 
-	// NOTE: cannot reference a field "before" it is defined.
-	private static final String tokenValidator = ELEMENT_AND_CONDITION_FOLLOWED_BY_DOM_NAV_EXTRACTOR_FIXED;
-	private static final String attributeValidator = CSS_CONDTION_EXTRACTOR_FIXED;
+//@formatter:off
+	private static final String tokenValidator = "^([^ ~+>\\[]*(?:\\[[^\\]]+\\])*)($|\\s*[ ~+>]\\s*[^ ~+>\\[].*$)";
+//@formatter:on
+//@formatter:off
+	private static final String attributeValidator = "(?i)^(-?[_a-z]+[_a-z0-9-]*|\\*)?(#[_a-z0-9-]*)?(\\.[_a-z0-9-]*)?(:[a-z][a-z\\-]*\\([^)]+\\))?(\\[\\s*-?[_a-z]+[_a-z0-9-]*\\s*(\\=|\\~=|\\|=|\\^=|\\$=|\\*=)?\\s*([\"'][-_.#a-z0-9:\\/ ]+[\"']|[-_.#a-z0-9:\\/]+)?\\s*\\])*$";
+//@formatter:on
 
 	// control logging
 	private boolean reportedTokenValidator = false;
@@ -119,7 +71,7 @@ public class CssValidator {
 	}
 
 	private static boolean reportedNavSeparator = false;
-	private final String navSeparator = DOM_NAV_RE;
+	private final String navSeparator = "[ ~+>]";
 
 	public String getNavSeparator() {
 		if (debug) {
--- java/src/main/java/com/github/sergueik/selenium/XPathValidator.java
+++ java/src/main/java/com/github/sergueik/selenium/XPathValidator.java
@@ -31,60 +31,6 @@ public class XPathValidator {
 		return instance;
 	}
 
-	// origin: CssSelectorStringSplitter.java
-	// The following appears, because of excessive grouping
-	// to neither work too well nor is really needed for task at hand
-
-	private static final String DOM_NAVS = " /";
-	private static final String DOM_NAV_RE = "(?://?)";
-	private static final String ROOT_DOM_NAV_RE = "(?:/?/?)";
-
-	// @formatter:off
-	private static final String ELEMENT_WITH_CONDITION = "([^" + DOM_NAVS + "\\[]*(\\[[^\\]]+\\])*)";
-	private static final String ELEMENT_WITH_CONDITION_FOLLOWED_BY_DOM_NAV_EXTRACTOR = "^" + ELEMENT_WITH_CONDITION + "($|(\\s*(" + DOM_NAV_RE + ")\\s*" + "([^" + DOM_NAVS + "].*)$))";
-	// @formatter:on
-
-	// Shortened grammar expression:
-
-	// @formatter:off
-	private static final String ELEMENT_WITH_CONDITION_FOLLOWED_BY_DOM_NAV_EXTRACTOR_FIXED = "^\\s*(/?/?\\s*[^ /\\[]+(?:\\[[^\\]]+\\])*)($|\\s*//?\\s*[^ /\\[]+.*$)";
-	// @formatter:on
-
-	// copied from: CssElementAttributeParser.java
-	// may be unneeded, currently unused, need more work
-
-	private static final String QUOTES_RE = "([\"\'])";
-	private static final String ATTRIBUTE_VALUE_RE = "([-_.#a-zA-Z0-9:\\/ ]+)";
-	private static final String ATTRIBUTE_VALUE_RE_NO_SPACES = "([-_.#a-zA-Z0-9:\\/]+)";
-	private static final String ATTRIBUTE_TYPE_RE = createElementAttributeNameRegularExpression();
-	private static final String ELEMENT_ATTRIBUTE_NAME_RE = "(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)";
-	private static final String STARTING_ELEMENT_RE = "^("
-			+ ELEMENT_ATTRIBUTE_NAME_RE + "|([*]))?";
-	private static final String PSUEDO_RE = "(:[a-z][a-z\\-]*([(][^)]+[)])?)";
-	private static final String ATTRIBUTE_RE = "(" + PSUEDO_RE + "|(\\[" + "\\s*"
-			+ ELEMENT_ATTRIBUTE_NAME_RE + "\\s*" + ATTRIBUTE_TYPE_RE + "\\s*" + "(("
-			+ QUOTES_RE + ATTRIBUTE_VALUE_RE + QUOTES_RE + ")|("
-			+ ATTRIBUTE_VALUE_RE_NO_SPACES + "))?" + "\\s*" + "\\]))";
-
-	private static final String XPATH_ATTRIBUTE_PATTERN = STARTING_ELEMENT_RE
-			+ ATTRIBUTE_RE + "*$";
-
-	private static String createElementAttributeNameRegularExpression() {
-		StringBuilder builder = new StringBuilder();
-		for (CssAttributeValueType type : CssAttributeValueType.values()) {
-			if (builder.length() == 0) {
-				builder.append("((");
-			} else {
-				builder.append(")|(");
-			}
-			builder.append("\\").append(type.getEqualStringName());
-		}
-		builder.append("))?");
-		// System.out.println("elementAttributeRE="+builder);
-		return builder.toString();
-	}
-
-	// end imported code
 	@SuppressWarnings("unused")
 	private boolean debug = false;
 
@@ -92,11 +38,10 @@ public class XPathValidator {
 		this.debug = value;
 	}
 
-	// NOTE: cannot reference a field "before" it is defined.
-	// private static final String tokenValidator =
-	// ELEMENT_WITH_CONDITION_FOLLOWED_BY_DOM_NAV_EXTRACTOR;
-	private static final String tokenValidator = ELEMENT_WITH_CONDITION_FOLLOWED_BY_DOM_NAV_EXTRACTOR_FIXED;
-	private static final String attributeValidator = XPATH_ATTRIBUTE_PATTERN;
+// @formatter:off
+	private static final String tokenValidator = "^\\s*(/?/?\\s*[^ /\\[]+(?:\\[[^\\]]+\\])*)($|\\s*//?\\s*[^ /\\[]+.*$)";
+// @formatter:on
+	private static final String attributeValidator = null;
 
 	// control logging
 	private boolean reportedTokenValidator = false;
